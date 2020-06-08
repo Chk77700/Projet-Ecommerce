99b92e9d9b2880f7c7082c538641005994518ea0
christian.leng <christian.leng@epitech.eu>
2020-06-08 17:48:02 +0200

    test de christian

---
+++ src/components/testChristian.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class testChristian extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Bonjour de chistian</h1>
+            </div>
+        )
+    }
+}
