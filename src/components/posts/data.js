87ac96daba15789abdade408537c58b7eb8741fa
christian.leng <christian.leng@epitech.eu>
2020-06-11 13:09:16 +0200

    le fichier post ou il y a des donnees json qui me sert juste a test le test

---
+++ src/components/posts/data.js
@@ -0,0 +1,72 @@
+export const storeProducts = [
+    {
+        id: 1,
+        name: "Figurine",
+        createur: "Luc",
+        price: 15,
+        description: "Sin autem ad adulescentiam perduxissent, dirimi tamen interdum, Sin autem ad adulescentiam perduxissent, dirimi tamen interdum",
+        photo: "https://fc03.deviantart.net/fs71/f/2014/104/f/3/dark_future_warrior_3d_printed_action_figure_by_hauke3000-d7eg1n9.jpg",
+        inShop: false,
+        count: 0,
+        total: 0
+    },    
+    {
+        id: 2,
+        name: "Visiere",
+        createur: "Jack",
+        price: 5,
+        description: "Sin autem ad adulescentiam perduxissent, dirimi tamen interdum",
+        photo: "https://ville-nogentsurmarne.com/wp-content/uploads/2020/04/9ba782ae4cc816061dc8ba40938a0a58.jpg",
+        inShop: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        name: "Porte crayons",
+        createur: "Jean",
+        price: 7,
+        description: "Sin autem ad adulescentiam perduxissent, dirimi tamen interdum",
+        photo: "https://lh3.googleusercontent.com/proxy/mZlGZZpC_-kgy6qarohKxwUvEstmzhFC3iIO-fZApL-To9sGXdtmFsEgdWwFr4a3qnUiO9cTTNitBE-f",
+        inShop: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        name: "Support telephone",
+        createur: "Pierre",
+        price: 11,
+        description: "Sin autem ad adulescentiam perduxissent, dirimi tamen interdum",
+        photo: "https://i.etsystatic.com/17285982/r/il/7ce614/1823017499/il_570xN.1823017499_pur9.jpg",
+        inShop: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 5,
+        name: "PhoneGadjet",
+        photo: "https://i.all3dp.com/cdn-cgi/image/fit=cover,w=1284,h=722,gravity=0.5x0.5,format=auto/wp-content/uploads/2018/05/26142617/50-things.jpg",
+        urgences: null,
+        covid: null,
+        articles: "articles",
+        inShop: false,
+        count: 0,
+        total: 0
+    }
+]
+
+export const detailProduct  = [
+    {
+        id: 1,
+        name: "Figurine",
+        createur: "Luc",
+        price: 15,
+        description: "Sin autem ad adulescentiam perduxissent, dirimi tamen interdum",
+        photo: "https://fc03.deviantart.net/fs71/f/2014/104/f/3/dark_future_warrior_3d_printed_action_figure_by_hauke3000-d7eg1n9.jpg",
+        inShop: false,
+        count: 0,
+        total: 0
+    }
+]
+
