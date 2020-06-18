b1eea873839a2caae41c3de68be0a6096f0a4696
meir <meir.belaich@epitech.eu>
2020-06-18 16:25:28 +0200

    rabais

--- src/components/AnnonceForm.jsx
+++ src/components/AnnonceForm.jsx
@@ -1,65 +1,86 @@
 import React from "react";
-import {Container, Form, Row, Col, Button} from "react-bootstrap";
+import {useParams} from "react-router-dom";
+import {Button, Col, Container, Form, Row} from "react-bootstrap";
 import Axios from "axios";
 
-export default class AnnonceForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            description: null,
-            name: null,
-            stock: null,
-            price: null,
-            photo: null
-        }
-    }
+export default function Modify() {
+    let {id} = useParams();
+    let state = {
+        description: null,
+        name: null,
+        stock: null,
+        price: null,
+        photo: null
+    };
+    let pourcent = [];
+    for (let i = 0; i < 100; i++)
+        pourcent.push(true);
 
-    sendForm = async (e) => {
+    const sendForm = async (e) => {
         e.preventDefault();
         let form = new FormData();
-        form.append("name", this.state.name);
-        form.append("description", this.state.description);
-        form.append("stock", this.state.stock);
-        form.append("price", this.state.price);
-        form.append("photo", this.state.photo);
+        form.append("name", state.name);
+        form.append("description", state.description);
+        form.append("stock", state.stock);
+        form.append("price", state.price);
+        form.append("photo", state.photo);
         form.append("id_vendeur", localStorage.getItem("userId"));
-        const response = await Axios.post("http://localhost:8000/postArticle", form);
+        form.append("id", id)
+        const response = await Axios.post("http://localhost:8000/modifyArticle", form);
         window.location = "http://localhost:3000/maBoutique";
     }
 
-    render() {
-        return (
-            <Container style={{marginTop: "20px"}}>
-                <Form>
-                    <Row>
-                        <Col>
-                            <Form.Control required placeholder="Nom de l'article"
-                                          onChange={val => this.setState({name: val.target.value})}/>
-                        </Col>
-                        <Col>
-                            <Form.Control required type={"number"} placeholder="Prix"
-                                          onChange={val => this.setState({price: val.target.value})}/>
-                        </Col>
-                    </Row>
-                    <Form.Control required style={{marginTop: "20px"}} as="textarea" rows="3"
-                                  onChange={val => this.setState({description: val.target.value})}/>
-                    <Row style={{marginTop: "20px"}}>
-                        <Col>
-                            <Form.Control required type={"number"} placeholder="Stock"
-                                          onChange={val => this.setState({stock: val.target.value})}/>
-                        </Col>
-                        <Col>
-                            <Form.File required name="Photo" label="Photo"
-                                       onChange={val => this.setState({photo: val.target.files[0]})}/>
-                        </Col>
-                        <Col>
-                            <Button type={"submit"} variant={"ecommerce3"} onClick={this.sendForm}>
-                                Valider
-                            </Button>
-                        </Col>
-                    </Row>
-                </Form>
-            </Container>
-        )
+    const modifySale = async (e) => {
+        const res = await Axios.post("http://localhost:8000/modifySale", {id: id, pourcentage: e.target.value});
+        if (res.data)  window.location = "http://localhost:3000/maBoutique";
     }
+
+    return (
+        <Container style={{marginTop: "20px"}}>
+            <Form>
+                <Row>
+                    <Col>
+                        <Form.Control required placeholder="Nom de l'article"
+                                      onChange={val => state.name = val.target.value}/>
+                    </Col>
+                    <Col>
+                        <Form.Control required type={"number"} placeholder="Prix"
+                                      onChange={val => state.price = val.target.value}/>
+                    </Col>
+                </Row>
+                <Form.Control required style={{marginTop: "20px"}} as="textarea" rows="3"
+                              onChange={val => state.description = val.target.value}/>
+                <Row style={{marginTop: "20px"}}>
+                    <Col>
+                        <Form.Control required type={"number"} placeholder="Stock"
+                                      onChange={val => state.stock = val.target.value}/>
+                    </Col>
+                    <Col>
+                        <Form.File required name="Photo" label="Photo"
+                                   onChange={val => state.photo = val.target.files[0]}/>
+                    </Col>
+                    <Col>
+                        <Button type={"submit"} variant={"ecommerce3"} onClick={sendForm}>
+                            Valider
+                        </Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        Pourcentage de rectuction:
+                    </Col>
+                    <Col>
+                        <Form.Control as="select" onChange={(e) => modifySale(e)} custom>
+                            <option>Choisissez le pourcentage de reduction</option>
+                            {
+                                pourcent.map((x, i) => (
+                                    <option value={i}>{`${i}%`}</option>
+                                ))
+                            }
+                        </Form.Control>
+                    </Col>
+                </Row>
+            </Form>
+        </Container>
+    );
 }
