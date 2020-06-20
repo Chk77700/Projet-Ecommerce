856d764e82bfde8ecce654ce7e4f9c79dd47d9c6
zacharia <zacharia.attik@epitech.eu>
2020-06-20 18:23:45 +0200

    create method return jsondata with axios/promise

--- src/components/MesCommandes.jsx
+++ src/components/MesCommandes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Container, Card, Col, Button, Row} from "react-bootstrap";
+import {Container, Card, Col, Button, Row, FormControl, Form} from "react-bootstrap";
 import Axios from "axios";
 import {Link} from "react-router-dom";
 
@@ -7,36 +7,71 @@ export default class MesCommandes extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            commandes: []
+            commandes: [],
+            search: {},
+            isFind: false,
+            id: localStorage.getItem("userId"),
+            searchTerm: ""
         }
     }
 
     componentDidMount() {
         this.getCommandes()
+
     }
 
     getCommandes = async () => {
-        const commandes = await Axios.post("http://localhost:8000/mesCommandes", {id: localStorage.getItem("userId")});
+        const commandes = await Axios.post("http://localhost:8000/mesCommandes", {id: this.state.id});
         console.log(commandes.data);
         this.setState({commandes: commandes.data});
-    }
+    };
+    getSearchOrder = async (e) => {
+        e.preventDefault();
+
+        const search = await Axios.post("http://localhost:8000/searchOrder", {searchTerm: this.state.searchTerm})
+            .then((response) => {
+                this.setState({
+                    search: response.data,
+                    isFind: true,
+                });
+                console.table(this.state.search)
+                console.log(typeof (this.state.search))
+            }, (error) => {
+                console.log("caca")
+                console.table("Error", this.state.search);
+                console.log(error)
+            });
+    };
 
     render() {
+        let result = this.state.search;
+        const isFind = this.state.isFind;
+
         return (
             <Container>
+                <Form inline>
+                    <FormControl type="text" onChange={(e) => this.setState({searchTerm: e.target.value})}
+                                 placeholder="Recherchez un objet" className="mr-sm-2"/>
+                    <Link to={this.state.search}>
+                        <Button onClick={this.getSearchOrder}>rechercher</Button>
+                    </Link>
+                </Form>
                 <h3 className={"text-ecommerce3"}>
                     Recapitulatif de vos commandes:
                 </h3>
                 <br/>
+
                 {this.state.commandes.length === 0 && <h4 className={"text-danger"}>Aucunes commandes</h4>}
+                {this.state.search === 0 && <h4 className={"text-danger"}>Aucun resultat</h4>}
+
                 {
-                    this.state.commandes.map((x, i) => (
+                    this.state.commandes.map((x, i) =>
                         <>
-                            <Card>
+                            <Card key={i}>
                                 <Card.Header>
                                     <h3>{`Statut: ${x.statut}`}</h3>
                                 </Card.Header>
-                                <Card.Body key={i} className={"text-ecommerce2"}>
+                                <Card.Body className={"text-ecommerce2"}>
                                     <h3>{`Boutique: ${x.boutique}`}</h3>
                                     <h4>{`Adresse: ${x.adresse} ${x.code_postal} ${x.ville}`}</h4>
                                     <h4>{`Total: ${x.prix_total}€`}</h4>
@@ -56,10 +91,39 @@ export default class MesCommandes extends React.Component {
                                     </Row>
                                 </Card.Footer>
                             </Card>
-                            <br/>
                         </>
-                    ))
+                    )}
+
+                {isFind &&
+                <>
+                    <Card>
+                        <Card.Header>
+                            <h3>{`Statut: ${result.statut}`}</h3>
+                        </Card.Header>
+                        <Card.Body className={"text-ecommerce2"}>
+                            <h3>{`Boutique: ${result.sell_name}`}</h3>
+                            <h4>{`Adresse: ${result.adresse} ${result.code_postal} ${result.ville}`}</h4>
+                            <h4>{`Total: ${result.prix_total}€`}</h4>
+                        </Card.Body>
+                        <Card.Footer>
+                            <Row>
+                                <Col>
+                                    <h5>{`Date: ${result.date}`}</h5>
+                                </Col>
+                                <Col>
+                                    <Link to={`/commandeDetail/${result.id}`}>
+                                        <Button variant={"ecommerce3"}>
+                                            Details
+                                        </Button>
+                                    </Link>
+                                </Col>
+                            </Row>
+                        </Card.Footer>
+                    </Card>
+                    <br/>
+                </>
                 }
+
             </Container>
         )
     }
