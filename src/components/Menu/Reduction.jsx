0f9223b0f08100f54e56d5aaded0ed85ccc2614e
mohammed <mohammed.benhelli@epitech.eu>
2020-06-26 18:32:24 +0200

    push clean

---
+++ src/components/Menu/Reduction.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Reduction extends Component {
+    render () {
+        return (
+            <div>
+                <p>bonjour Reduction</p>
+            </div>
+        )
+    }
+}
