82b0c44a192a1b6fcf1c7ddf0ce59292ac344165
christian.leng <christian.leng@epitech.eu>
2020-06-27 19:27:39 +0200

    modification de text

--- src/components/Boutiques.jsx
+++ src/components/Boutiques.jsx
@@ -84,7 +84,7 @@ export default class Boutiques extends React.Component {
     render() {
         return (
             <Container className="ma-boutique-padding">
-                <p className="boutiques-p-padding">Vous pouvez consulter tous nos vendeurs :</p>
+                <p className="boutiques-p-padding">Liste de tous les vendeurs de Print'n go :</p>
                 <Row>
                     {
                         this.state.showBoutiques && this.state.boutiques.map((x, i) => (
